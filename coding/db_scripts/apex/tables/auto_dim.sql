dd34c5f98b97680ec111b9e8cda9aba0821124ac
Клейн Александр Маркович <AKleyn@alfabank.ru>
2019-02-26 12:50:27 +0500

    Added more detail fields

--- coding/db_scripts/apex/tables/auto_dim.sql
+++ coding/db_scripts/apex/tables/auto_dim.sql
@@ -10,25 +10,25 @@ exception
         dbms_output.put_line('Ошибка '||sqlerrm);
 end;
 /
-create table auto_dim (uk               number not null,
-                       model_uk         number,
-                       car_plate        varchar2(8),
-                       vin              varchar2(32),
-                       mileage          number,
-                       year_of_creation date,
-                       client_uk        number)
+create table auto_dim (uk         number not null,
+                       model_uk   number,
+                       car_plate  varchar2(16),
+                       vin        varchar2(32),
+                       mileage    number,
+                       month_year varchar2(8),
+                       client_uk  number)
 tablespace apex;
 
 comment on table auto_dim is 'Список автомобилей клиентов';
 
-comment on column auto_dim.uk               is 'Уникальный ключ';
-comment on column auto_dim.model_uk         is 'Модель';
-comment on column auto_dim.car_plate        is 'Государственный номер';
-comment on column auto_dim.vin              is 'VIN';
-comment on column auto_dim.mileage          is 'Пробег';
-comment on column auto_dim.year_of_creation is 'Год выпуска';
-comment on column auto_dim.client_uk        is 'Владелец';
+comment on column auto_dim.uk         is 'Уникальный ключ';
+comment on column auto_dim.model_uk   is 'Модель';
+comment on column auto_dim.car_plate  is 'Государственный номер';
+comment on column auto_dim.vin        is 'VIN';
+comment on column auto_dim.mileage    is 'Пробег';
+comment on column auto_dim.month_year is 'Год выпуска';
+comment on column auto_dim.client_uk  is 'Владелец';
 
-create unique index uk_auto_dim on auto_dim
+create unique index apex.uk_auto_dim on apex.auto_dim
 (uk)
 tablespace apex;
