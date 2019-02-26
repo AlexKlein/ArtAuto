7294055f95bd81598c453ec5e97712dfb9474a2e
Клейн Александр Маркович <AKleyn@alfabank.ru>
2019-02-26 19:08:34 +0500

    Initial loading

---
+++ coding/db_scripts/apex/scripts/script_service_dim.sql
@@ -0,0 +1,8 @@
+﻿insert into service_dim (name,
+                         address,
+                         service_phone,
+                         order_phone) values ('"ООО Арт-Авто"',
+                                              '620055, РОССИЯ, г. Екатеринбург, ул. 8-марта, д.267г',
+                                              '(343)-202-65-65',
+                                              '(343)-202-05-08');
+commit;
